288b1bc62f1d744ae68cee80dc0abd7de9440cc7
Dayne Broderson <broderson@gmail.com>
2013-11-09 14:01:32 -0900

    converting anArray to @heroes in Combat

--- lib/combat.rb
+++ lib/combat.rb
@@ -3,18 +3,19 @@ class Combat
   # This needs some rework its gotten fat and clumsy, but it works...
   # I should include a transcript of the combat to be saved to a text file just for fun
 
-  def initialize( anArray, fallen, boolSpar )
-     combat( anArray, fallen, boolSpar )
+  def initialize( heroes, fallen, boolSpar )
+    combat(fallen, boolSpar )
+    @heroes = heroes
   end
 
-  def combat( anArray, fallen, boolSpar )
+  def combat(fallen, boolSpar )
     @spar = boolSpar
-    @range = anArray.length
+    @range = @heroes.length
     if @range <= 1
       puts 'There are not enough available combatants'
     else
       puts '<<The Heroes>>'
-      lineup(anArray)
+      lineup(@heroes)
       puts ''
       valid_choice = false
 #---attempt to parse an input and use it as the choice
@@ -22,27 +23,27 @@ class Combat
         puts 'First combatant'
         c1 = gets.chomp.to_i - 1
 #---	@good = false
-#---	anArray.each.first.any? { |w| c1 =~ /#{w}/  }
+#---	@heroes.each.first.any? { |w| c1 =~ /#{w}/  }
         puts 'Second combatant'
         c2 = gets.chomp.to_i - 1
 	c2 = gets.chomp
         if c2 == c1
-    puts anArray[c1].show_name + ' cannot fight themselves! Choose again.'
+    puts @heroes[c1].show_name + ' cannot fight themselves! Choose again.'
 	else
 	  valid_choice = true
 	end
       end
 
       #need to think of a better health system, but I want multiple hits to win for now...      
-      @c1health = anArray[c1].get_level + 1
-      @c2health = anArray[c2].get_level + 1
+      @c1health = @heroes[c1].get_level + 1
+      @c2health = @heroes[c2].get_level + 1
 
       puts 'Let the battle begin!'
       puts ''
       victory = false
       while (not victory)
-	@c1a = attack( anArray[c1] )
-	@c2a = attack( anArray[c2] )
+	@c1a = attack( @heroes[c1] )
+	@c2a = attack( @heroes[c2] )
 	print '(' + @c1a.to_s + 'x' + @c2a.to_s + ')'
 
         #need to read more on comparable module, might make a lot more sense to use it here
@@ -52,31 +53,31 @@ class Combat
 	  puts 'It\'s that moment in a fight when they bring their weapons together and glare at one another face to face'
 	elsif @c1a > @c2a
 	  @c2health -= 1
-	  puts anArray[c1].first_name + ' wounds ' + anArray[c2].first_name
+	  puts @heroes[c1].first_name + ' wounds ' + @heroes[c2].first_name
 	  if @c2health < 0
             puts ''
-	    puts anArray[c1].show_name + ' strikes down ' + anArray[c2].show_name
+	    puts @heroes[c1].show_name + ' strikes down ' + @heroes[c2].show_name
 	    if @spar == false
-	      puts 'Give \'' + anArray[c1].show_name + '\' an addtion to his name'
+	      puts 'Give \'' + @heroes[c1].show_name + '\' an addtion to his name'
 	      input = gets.chomp
-	      anArray[c1].add_name(input)
-	      fallen.push(anArray[c2])
-	      anArray.delete_at(c2)
+	      @heroes[c1].add_name(input)
+	      fallen.push(@heroes[c2])
+	      @heroes.delete_at(c2)
 	    end
 	    victory = true
 	  end
 	elsif @c1a < @c2a
 	  @c1health -= 1
-	  puts anArray[c2].first_name + ' wounds ' + anArray[c1].first_name
+	  puts @heroes[c2].first_name + ' wounds ' + @heroes[c1].first_name
 	  if @c1health < 0
             puts ''
-	    puts anArray[c2].show_name + ' strikes down ' + anArray[c1].show_name
+	    puts @heroes[c2].show_name + ' strikes down ' + @heroes[c1].show_name
 	    if @spar == false
-	      puts 'Give \'' + anArray[c2].show_name + '\' an addtion to his name'
+	      puts 'Give \'' + @heroes[c2].show_name + '\' an addtion to his name'
 	      input = gets.chomp
-	      anArray[c2].add_name(input)
-	      fallen.push(anArray[c1])
-	      anArray.delete_at(c1)
+	      @heroes[c2].add_name(input)
+	      fallen.push(@heroes[c1])
+	      @heroes.delete_at(c1)
 	    end
 	    victory = true
 	  end
